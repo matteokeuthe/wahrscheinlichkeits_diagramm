277904c7ed54b998294616b8c8dcfaf172690ffd
matteo <matteo.keuthe@gmail.com>
2026-01-13 19:37:46 +0100

    added factorial function

---
+++ binomial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    """Berechnet n! (Fakultät von n)"""
+    if n < 0:
+        raise ValueError("n muss >= 0 sein")
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
